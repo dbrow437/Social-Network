a75329a514ecaef7f2a3579ef45cdd6a9e5cbed1
Damon Brown <dbrow437@gmail.com>
2018-07-26 17:40:06 -0700

    User API routes & JWT Auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://damon:password1@ds143511.mlab.com:43511/mernexample"
+  mongoURI: "mongodb://damon:password1@ds143511.mlab.com:43511/mernexample",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -7,6 +9,9 @@ const posts = require("./routes/api/posts");
 
 const app = express();
 
+//body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 //db config
 const db = require("./config/keys").mongoURI;
 
@@ -16,8 +21,11 @@ mongoose
   .then(() => console.log("mongoDB Connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello!"));
+//passport middleware
+app.use(passport.initialize());
 
+//passport config
+require("./config/passport")(passport);
 //use routes
 app.use("/api/users", users);
 app.use("/api/profile", profile);
