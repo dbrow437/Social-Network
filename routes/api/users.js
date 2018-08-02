d062ed73b6f2165894be82af5161ba0bb31d3d89
Damon Brown <dbrow437@gmail.com>
2018-08-02 15:04:28 -0700

    profile models and routes

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,7 +11,7 @@ const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 
 //load user model
-const User = require("../models/User");
+const User = require("../../models/User");
 
 //@route  GET api/users/test
 //@desc   Test users routes
