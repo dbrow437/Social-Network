e365f48a8b14d19f433122202eb3f17ebc37f430
Damon Brown <dbrow437@gmail.com>
2018-08-06 19:39:19 -0700

    get and delete posts

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 //Create Schema
-const PostSchema = new Schema ({
+const PostSchema = new Schema({
   user: {
-    type: Schema.Types.ObjecId
-    ref: 'users'
+    type: Schema.Types.ObjectId,
+    ref: "users"
   },
   text: {
     type: String,
@@ -21,7 +21,7 @@ const PostSchema = new Schema ({
     {
       user: {
         type: Schema.Types.ObjectId,
-        ref: 'users'
+        ref: "users"
       }
     }
   ],
@@ -29,7 +29,7 @@ const PostSchema = new Schema ({
     {
       user: {
         type: Schema.Types.ObjectId,
-        ref: 'users'
+        ref: "users"
       },
       text: {
         type: String,
@@ -51,7 +51,6 @@ const PostSchema = new Schema ({
     type: Date,
     default: Date.now
   }
-})
+});
 
-
-module.exports = Post = mongoose.model('post', PostSchema)
+module.exports = Post = mongoose.model("post", PostSchema);
