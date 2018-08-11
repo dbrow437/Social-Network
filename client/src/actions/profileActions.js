c2f3c124257dad265c27c877312d4f4b952e9868
Damon Brown <dbrow437@gmail.com>
2018-08-11 16:39:43 -0700

    add and delete experience and education

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -39,6 +39,66 @@ export const createProfile = (profileData, history) => dispatch => {
     );
 };
 
+//add experience
+export const addExperience = (expData, history) => dispatch => {
+  axios
+    .post("/api/profile/experience", expData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+//Add education
+export const addEducation = (eduData, history) => dispatch => {
+  axios
+    .post("/api/profile/education", eduData)
+    .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+//Delete Experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .post(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+//Delete Education
+export const deleteEducation = id => dispatch => {
+  axios
+    .post(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 //Delete account & profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm("Are you sure?  This CANNOT be undone!")) {
